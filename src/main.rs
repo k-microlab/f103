5c6641a85a88572538db7f3b3760782e3d73c7dc
vkazbanov <vkazbanov@telematika.com>
2025-04-16 17:58:15 +0200

    embassy

--- src/main.rs
+++ src/main.rs
@@ -1,121 +1,45 @@
-// #![deny(unsafe_code)]
 #![no_std]
 #![no_main]
 
-use core::ops::Mul;
-use defmt_rtt as _;
-use panic_probe as _;
-
+use core::fmt::Write;
 use cortex_m_rt::entry;
-use stm32f1xx_hal::{pac, prelude::*};
-
-use stm32f1xx_hal::i2c::{BlockingI2c, DutyCycle, Mode};
-
-use embedded_graphics::{
-    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
-    pixelcolor::BinaryColor,
-    prelude::*,
-    text::{Baseline, Text},
-};
-use embedded_graphics::primitives::{Circle, PrimitiveStyle, PrimitiveStyleBuilder, Rectangle, StrokeAlignment, Triangle};
-use micromath::F32;
-use ssd1306::{mode::BufferedGraphicsMode, prelude::*, I2CDisplayInterface, Ssd1306};
-use stm32f1xx_hal::adc::{Adc, SampleTime};
+use defmt_rtt as _;
+use embassy_stm32::time::Hertz;
+use embassy_stm32::{bind_interrupts, i2c, peripherals};
+use panic_probe as _;
+use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
 
 #[entry]
 fn main() -> ! {
-    // Get access to the core peripherals from the cortex-m crate
-    let core = cortex_m::Peripherals::take().unwrap();
-    // Get access to the device specific peripherals from the peripheral access crate
-    let device = pac::Peripherals::take().unwrap();
+    let p = embassy_stm32::init(Default::default());
 
-    // Take ownership over the raw flash and rcc devices and convert them into the corresponding
-    // HAL structs
-    let mut flash = device.FLASH.constrain();
-    let rcc = device.RCC.constrain();
+    bind_interrupts!(struct Irqs {
+        I2C1_EV => i2c::EventInterruptHandler<peripherals::I2C1>;
+        I2C1_ER => i2c::ErrorInterruptHandler<peripherals::I2C1>;
+    });
 
-    defmt::info!("Loading");
-
-    // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
-    // `clocks`
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
-
-    let mut afio = device.AFIO.constrain();
-
-    /*// Acquire the GPIOC peripheral
-    let mut gpioc = dp.GPIOC.split();
-
-    // Configure gpio C pin 13 as a push-pull output. The `crh` register is passed to the function
-    // in order to configure the port. For pins 0-7, crl should be passed instead.
-    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-    // Configure the syst timer to trigger an update every second
-    let mut timer = Timer::syst(cp.SYST, &clocks).counter_hz();
-    timer.start(1.Hz()).unwrap();
-
-    // Wait for the timer to trigger an update and change the state of the LED
-    loop {
-        block!(timer.wait()).unwrap();
-        led.set_high();
-        block!(timer.wait()).unwrap();
-        led.set_low();
-    }*/
-
-    // Setup pins
-    let mut gpioa = device.GPIOA.split();
-
-    let mut adc_ch0 = gpioa.pa0.into_analog(&mut gpioa.crl);
-
-    let mut gpiob = device.GPIOB.split();
-
-    let mut adc = Adc::adc1(device.ADC1, clocks);
-    adc.set_sample_time(SampleTime::T_239);
-
-    let scl = gpiob.pb6.into_alternate_open_drain(&mut gpiob.crl);
-    let sda = gpiob.pb7.into_alternate_open_drain(&mut gpiob.crl);
-
-    let i2c = BlockingI2c::i2c1(
-        device.I2C1,
-        (scl, sda),
-        &mut afio.mapr,
-        Mode::Fast {
-            frequency: 400.kHz(),
-            duty_cycle: DutyCycle::Ratio16to9,
-        },
-        clocks,
-        1000,
-        10,
-        1000,
-        1000,
+    let i2c = embassy_stm32::i2c::I2c::new(
+        p.I2C1,
+        p.PB6,
+        p.PB7,
+        Irqs,
+        p.DMA1_CH6,
+        p.DMA1_CH7,
+        Hertz::khz(400),
+        Default::default(),
     );
 
     let interface = I2CDisplayInterface::new(i2c);
-    let mut display = Ssd1306::new(
-        interface,
-        DisplaySize128x32,
-        DisplayRotation::Rotate0,
-    ).into_buffered_graphics_mode();
+    let mut display =
+        Ssd1306::new(interface, DisplaySize128x32, DisplayRotation::Rotate0).into_terminal_mode();
     display.init().unwrap();
-
-    let border_stroke = PrimitiveStyleBuilder::new()
-        .stroke_color(BinaryColor::On)
-        .stroke_width(3)
-        .stroke_alignment(StrokeAlignment::Inside)
-        .build();
-    let fill = PrimitiveStyle::with_fill(BinaryColor::On);
-
+    let _ = display.clear();
+    let _ = display.print_char('H');
+    let _ = display.print_char('e');
+    let _ = display.print_char('l');
+    let _ = display.print_char('l');
+    let _ = display.print_char('o');
+    // let _ = display.write_fmt(format_args!("{}", 10));
     loop {
-        let mut buffer = [0u16; 128];
-
-        for b in buffer.iter_mut() {
-            *b = adc.read(&mut adc_ch0).unwrap();
-        }
-
-        display.clear();
-        for (x, v) in buffer.iter().enumerate() {
-            let y = *v as u32 / 128;
-            display.set_pixel(x as u32, y, true);
-        }
-
-        display.flush().unwrap();
     }
 }
