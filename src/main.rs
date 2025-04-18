67dc211d73890b1d96ce14975ac0a541bcfea15d
vkazbanov <vkazbanov@telematika.com>
2025-04-18 23:52:39 +0200

    cef

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 #![no_std]
 #![no_main]
 
-use core::fmt::Write;
-use cortex_m_rt::entry;
+use defmt::info;
 use defmt_rtt as _;
+use embassy_executor::Spawner;
 use embassy_stm32::time::Hertz;
 use embassy_stm32::{bind_interrupts, i2c, peripherals};
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_time::Timer;
 use panic_probe as _;
 use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
 
-#[entry]
-fn main() -> ! {
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
 
     bind_interrupts!(struct Irqs {
@@ -39,7 +41,15 @@ fn main() -> ! {
     let _ = display.print_char('l');
     let _ = display.print_char('l');
     let _ = display.print_char('o');
+
+    let mut led = Output::new(p.PC13, Level::High, Speed::Low);
+    
     // let _ = display.write_fmt(format_args!("{}", 10));
     loop {
+        led.set_high();
+        Timer::after_millis(30).await;
+
+        led.set_low();
+        Timer::after_millis(30).await;
     }
 }
