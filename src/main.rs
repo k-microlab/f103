84a6a1302502f1797b0c9b7e5a6fe5aad240d66b
Radviger <radviger@gmail.com>
2026-04-18 13:21:14 +0200

    properly clear the display

--- src/main.rs
+++ src/main.rs
@@ -56,8 +56,9 @@ fn main() -> ! {
 
     defmt::info!("Display size: {:?}x{:?}", w, h);
 
-    disp.set_draw_area((0, 0), (w, h)).expect("Failed to set draw area");
-    disp.fill_solid(&Rectangle::new(Point::new(0, 0), Size::new(w as _, h as _)), Rgb565::GREEN).expect("Failed to fill solid area");
+    // disp.set_draw_area((0, 0), (w, h)).expect("Failed to set draw area");
+    // disp.fill_solid(&Rectangle::new(Point::new(0, 0), Size::new(w as _, h as _)), Rgb565::GREEN).expect("Failed to fill solid area");
+    <_ as DrawTarget>::clear(&mut disp, Rgb565::GREEN).expect("Failed to clear display");
 
     /*let bmp =
         Bmp::from_slice(include_bytes!("./image.bmp")).expect("Failed to load BMP image");
